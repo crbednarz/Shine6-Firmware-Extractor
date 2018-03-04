51cd3c3074cfc2b38767e6cd2f2eb525b34e923a
Skiphs <cr.bednarz@gmail.com>
2018-03-04 11:46:32 -0500

    Added basic argument validation & response.

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ Tool & Documentation for the extraction of Duck Shine 6's firmware from its firm
 After compiling the tool, you can use the first argument to specify the path, and the second to specify the output.
 By default it only extracts the US firmware from the Shine6 FWUpdate.exe for V1.02.10
 
+Example Usage: Shine6FWExtractor.exe FWUpdate.exe firmware.bin
+
 However, the intention of the tool is more to cover how one might approach extracting future firmware updates from Ducky devices.
 Comments in UpdaterConstants.cs may offer some insight on approach other versions of the updater.
+
 
 ## License
 License information can be found in the LICENSE file in the root directory.
--- Shine6FWExtractor/Program.cs
+++ Shine6FWExtractor/Program.cs
@@ -11,8 +11,23 @@ namespace Shine6FWExtractor
 	{
 		public static void Main(string[] args)
 		{
+			if (args.Length != 2)
+			{
+				Console.WriteLine(
+					"Shine6FWExtractor\n" +
+					"Usage: Shine6FWExtractor.exe [FWUpdate.exe path] [Output path]\n" +
+					"Example: Shine6FWExtractor.exe FWUpdate.exe firmware.bin");
+				return;
+			}
+
 			var pathToUpdater = args[0];
 			var outputPath = args[1];
+
+			if (!File.Exists(pathToUpdater))
+			{
+				Console.WriteLine("Failed to find FWUpdate.exe file. Path \"{0}\" not found.", pathToUpdater);
+				return;
+			}
 			
 			var reader = UpdaterReader.FromPath(pathToUpdater);
 
@@ -22,6 +37,8 @@ namespace Shine6FWExtractor
 			var firmware = Utilities.DecryptFirmware(encryptedFirmware);
 
 			File.WriteAllBytes(outputPath, firmware);
+
+			Console.WriteLine("Extraction complete!");
 		}
 	}
 }
